72334decdc560f65a873222d0809b31e156ba636
gclawson1 <clawsonsa1101@gmail.com>
2021-05-05 14:23:14 -0700

    fixed the FAO datasets

--- workflow/R/fao_fxn.R
+++ workflow/R/fao_fxn.R
@@ -32,3 +32,31 @@ fao_clean_data <- function(m, sub_0_0 = 0.1) {
   
   return(m1)
 }
+
+fao_clean_data_new <- function(m, sub_N = 0.1) {
+  ### Swaps out FAO-specific codes for analysis:
+  ### * FAO_commodities (Natural Products goal)
+  ###
+  ### Note separate calls to mutate() may not be necessary, but ensures proper sequence of flag-replacing, just in case...
+  ###
+  
+  m1 <- m %>%
+    mutate(  
+      value = str_replace(value, fixed('E '), ''), 
+      value = str_replace(value, fixed(' E'), ''),
+      ### FAO denotes with E when they have estimated the value using best available data,
+      ###   sometimes comes at start (commodities), sometimes at end (mariculture)...?
+      value = ifelse(value == '...', NA, value),
+      ### FAO's code for NA
+      value = str_replace(value, fixed(' N'), sub_N),  
+      ### FAO denotes something as 'N' when it is > 0 but < 1/2 of a unit. 
+      ### Replace with lowdata_value.
+      value = str_replace(value, fixed(  '-'), '0'),  
+      ### FAO's code for true 0
+      value = ifelse(value =='', NA, value)) %>%
+    mutate(
+      value = as.numeric(as.character(value)),
+      year  = as.integer(as.character(year)))       # search in R_inferno.pdf for "shame on you"
+  
+  return(m1)
+}
